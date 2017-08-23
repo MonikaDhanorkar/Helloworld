9dbc02ed93d63a90e157a5e7524e77b50d7a375b
MonikaDhanorkar <30587082+MonikaDhanorkar@users.noreply.github.com>
2017-08-23 13:05:16 +0530

    Sixth Commit

--- HelloWorld/src/Demo.java
+++ HelloWorld/src/Demo.java
@@ -8,9 +8,7 @@ for(int i=0;i<10;i++)
 			System.out.println("hello world..."+i);
 		}
 char array[] = {'a','b','g','j'};
-/*I'm displaying the value which does not
- * exist so this should throw an exception
- */
+
 System.out.println(array[78]);
 
 	}
